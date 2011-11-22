cbb78cc3ed022c3aef18d4613bcb21ebdac742ca
Dmitry Karpenko <dmitry.astu@gmail.com>
2011-11-22 18:20:45 +0600

    On the way

--- com.onpositive.mapper/src/tiled/io/MapHelper.java
+++ com.onpositive.mapper/src/tiled/io/MapHelper.java
@@ -1,7 +1,7 @@
 /*
  *  Tiled Map Editor, (c) 2004-2006
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
